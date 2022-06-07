b61f8fe8f612eef8a8474268633d051c695d6995
playmakermz <sayaalif101@gmail.com>
2022-06-07 19:28:50 +0700

    Perbaikan 01

--- application/views/V_Dashboard.php
+++ application/views/V_Dashboard.php
@@ -1,88 +1,229 @@
 <div class="beranda" id="beranda">
-    <div class="container">
+	<div class="container">
+        
+		<!-- Highlight -->
+		<h3 class="title mb-0 ">Selamat Datang, Masyarakat Desa Kumpulrejo </h3>
+		<h6 class="text-muted text-capitalize">Pelayanan Terpadu Masyarakat Desa Kumpulrejo</h6>
 
-        <!-- Highlight -->
-        <h3 class="title mb-0 ">Selamat Datang, Masyarakat Desa Kumpulrejo </h3>
-        <h6 class="text-muted text-capitalize">Pelayanan Terpadu Masyarakat Desa Kumpulrejo</h6>
-        <div class="row justify-content-center">
-            <div id="all_news" class="all_news"> <span>Pengumuman</span>
-                <ul>
-                    <li><a href="#">INFOGRAFIS APBDes 2021</a></li>
-                    <li><a href="#">Pendaftaran Kartu Prakerja Gelombang 13</a></li>
-                    <li><a href="#">Pejabat Pengelola Informasi Dan Dokumentasi (PPID)</a></li>
-                </ul>
-            </div>
-        </div>
+        <!-- papan pengumuman -->
+					<div class="row justify-content-center">
+						<div id="all_news" class="all_news"> <span>Pengumuman</span>
+							<ul>
+								<li><a href="#">INFOGRAFIS APBDes 2021</a></li>
+								<li><a href="#">Pendaftaran Kartu Prakerja Gelombang 13</a></li>
+								<li><a href="#">Pejabat Pengelola Informasi Dan Dokumentasi (PPID)</a></li>
+							</ul>
+						</div>
+					</div>
 
-        <div class="container">
+        <!-- Carousel landing page -->
+		<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+			<div class="carousel-indicators">
+				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+					aria-current="true" aria-label="Slide 1"></button>
+				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+					aria-label="Slide 2"></button>
+				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+					aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+					aria-label="Slide 3"></button>
 
-            <!-- Top News -->
-            <div class="card card-plain">
-                <div class="card-header">
-                    <h4 class="title">Berita Teratas</h4>
-                    <div class="row justify-content-center ml-2">
-                        <div id="top_news" class="top_news"><span>Berita Terkini</span>
-                            <ul>
-                                <li><a href="#">Pencairan BST Masa Bayar 12-13</a></li>
-                                <li><a href="#">Penerimaan Kartu Tani Tahun 2021 Tahap I</a></li>
-                                <li><a href="#">Santunan Kematian III PPDI Kecamatan Patebon</a></li>
-                                <li><a href="#">Covid 19 - Latest Update</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="row justify-content-center">
-                        <div class="col-md-4">
-                            <div class="card" style="width: 20rem;">
-                                <img src="<?= base_url('assets/img/1637217474IMG-20211118-WA0044.jpg') ?>" alt="News 1">
-                                <div class="card-body">
-                                    <h4 class="card-title">PERDA BUMDES DI CABUT</h4>
-                                    <a href="<?= base_url('News/PerdaBumdesDiCabut') ?>" class="btn btn-primary"> Baca Berita... </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card" style="width: 20rem;">
-                                <img src="<?= base_url('assets/img/15816474021575516106-962-333017.jpg') ?>" alt="News 1">
-                                <div class="card-body">
-                                    <h4 class="card-title">Jenis Pestisida, Cara Kerja Dan Daftar Pestisida Serta Strategi Pergilirannya</h4>
-                                    <a href="<?= base_url('News/JenisPestisidaCaraKerjaDanDaftarPestisidaSertaStrategiPergilirannya') ?>" class="btn btn-primary"> Baca Berita... </a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="card" style="width: 20rem;">
-                                <img src="<?= base_url('assets/img/158147671727756_620.jpg') ?>" alt="News 1">
-                                <div class="card-body">
-                                    <h4 class="card-title">Metode Pemberian Pupuk Pada Tanaman</h4>
-                                    <a href="<?= base_url('News/MetodePemberianPupukPadaTanaman') ?>" class="btn btn-primary"> Baca Berita... </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
+					aria-label="Slide 2"></button>
+				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="5"
+					aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="6"
+					aria-label="Slide 3"></button>
+                
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="7"
+					aria-label="Slide 2"></button>
+				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="8"
+					aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="9"
+					aria-label="Slide 3"></button>
 
-            <!-- News -->
-            <div class="card card-plain">
-                <div class="card-header">
-                    <h4 class="title">Galeri Desa</h4>
-                </div>
-                <div class="card-body">
-                    <img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(1).jpeg') ?>" alt="" style="height: 120px; width:auto;">
-                    <img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(2).jpeg') ?>" alt="" style="height: 120px; width:auto   ">
-                    <img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(3).jpeg') ?>" alt="" style="height: 120px; width:auto   ">
-                    <img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(4).jpeg') ?>" alt="" style="height: 120px; width:auto   ">
-                    <img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(5).jpeg') ?>" alt="" style="height: 120px; width:auto   ">
-                    <img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(6).jpeg') ?>" alt="" style="height: 120px; width:auto   ">
-                    <img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(7).jpeg') ?>" alt="" style="height: 120px; width:auto   ">
-                    <img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(8).jpeg') ?>" alt="" style="height: 120px; width:auto   ">
-                    <img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(9).jpeg') ?>" alt="" style="height: 120px; width:auto   ">
-                    <img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(10).jpeg') ?>" alt="" style="height: 120px; width:auto   ">
-                </div>
-            </div>
-        </div>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="10"
+					aria-label="Slide 2"></button>
+				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="11"
+					aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="12"
+					aria-label="Slide 3"></button>
 
-    </div>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="13"
+					aria-label="Slide 2"></button>
+				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="14"
+					aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="15"
+					aria-label="Slide 3"></button>
+
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="16"
+					aria-label="Slide 2"></button>
+				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="17"
+					aria-label="Slide 3"></button>
+                
+			</div>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img src="<?= base_url('assets/img/carousel-img/01.jpg') ?>" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/02.jpg') ?>" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/03.jpg') ?>" class="d-block w-100" alt="...">
+				</div>
+
+                <div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/04.png') ?>" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/05.png') ?>" class="d-block w-100" alt="...">
+				</div>
+                <div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/06.png') ?>" class="d-block w-100" alt="...">
+				</div>
+
+                <div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/07.png') ?>" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/08.jpg') ?>" class="d-block w-100" alt="...">
+				</div>
+                <div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/09.jpg') ?>" class="d-block w-100" alt="...">
+				</div>
+
+                <div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/10.jpg') ?>" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/11.jpeg') ?>" class="d-block w-100" alt="...">
+				</div>
+                <div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/12.jpg') ?>" class="d-block w-100" alt="...">
+				</div>
+
+                <div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/13.jpg') ?>" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/14.jpg') ?>" class="d-block w-100" alt="...">
+				</div>
+                <div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/15.jpg') ?>" class="d-block w-100" alt="...">
+				</div>
+
+                <div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/16.png') ?>" class="d-block w-100" alt="...">
+				</div>
+                <div class="carousel-item">
+					<img src="<?= base_url('assets/img/carousel-img/17.jpg') ?>" class="d-block w-100" alt="...">
+				</div>
+
+                
+			</div>
+			<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+				data-bs-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Previous</span>
+			</button>
+			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+				data-bs-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="visually-hidden">Next</span>
+			</button>
+		</div>
+
+
+		<div class="container">
+
+
+			<!-- Top News -->
+			<div class="card card-plain">
+				<div class="card-header">
+					<h4 class="title">Berita Teratas</h4>
+
+					
+
+
+					<div class="row justify-content-center ml-2">
+						<div id="top_news" class="top_news"><span>Berita Terkini</span>
+							<ul>
+								<li><a href="#">Pencairan BST Masa Bayar 12-13</a></li>
+								<li><a href="#">Penerimaan Kartu Tani Tahun 2021 Tahap I</a></li>
+								<li><a href="#">Santunan Kematian III PPDI Kecamatan Patebon</a></li>
+								<li><a href="#">Covid 19 - Latest Update</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="card-body">
+					<div class="row justify-content-center">
+						<div class="col-md-4">
+							<div class="card" style="width: 20rem;">
+								<img src="<?= base_url('assets/img/1637217474IMG-20211118-WA0044.jpg') ?>" alt="News 1">
+								<div class="card-body">
+									<h4 class="card-title">PERDA BUMDES DI CABUT</h4>
+									<a href="<?= base_url('News/PerdaBumdesDiCabut') ?>" class="btn btn-primary"> Baca
+										Berita... </a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card" style="width: 20rem;">
+								<img src="<?= base_url('assets/img/15816474021575516106-962-333017.jpg') ?>"
+									alt="News 1">
+								<div class="card-body">
+									<h4 class="card-title">Jenis Pestisida, Cara Kerja Dan Daftar Pestisida Serta
+										Strategi Pergilirannya</h4>
+									<a href="<?= base_url('News/JenisPestisidaCaraKerjaDanDaftarPestisidaSertaStrategiPergilirannya') ?>"
+										class="btn btn-primary"> Baca Berita... </a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="card" style="width: 20rem;">
+								<img src="<?= base_url('assets/img/158147671727756_620.jpg') ?>" alt="News 1">
+								<div class="card-body">
+									<h4 class="card-title">Metode Pemberian Pupuk Pada Tanaman</h4>
+									<a href="<?= base_url('News/MetodePemberianPupukPadaTanaman') ?>"
+										class="btn btn-primary"> Baca Berita... </a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- News -->
+			<div class="card card-plain main-02">
+				<div class="card-header">
+					<h4 class="title">Galeri Desa</h4>
+				</div>
+				<div class="card-body">
+					<img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(1).jpeg') ?>" alt=""
+						style="height: 120px; width:auto;">
+					<img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(2).jpeg') ?>" alt=""
+						style="height: 120px; width:auto   ">
+					<img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(3).jpeg') ?>" alt=""
+						style="height: 120px; width:auto   ">
+					<img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(4).jpeg') ?>" alt=""
+						style="height: 120px; width:auto   ">
+					<img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(5).jpeg') ?>" alt=""
+						style="height: 120px; width:auto   ">
+					<img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(6).jpeg') ?>" alt=""
+						style="height: 120px; width:auto   ">
+					<img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(7).jpeg') ?>" alt=""
+						style="height: 120px; width:auto   ">
+					<img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(8).jpeg') ?>" alt=""
+						style="height: 120px; width:auto   ">
+					<img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(9).jpeg') ?>" alt=""
+						style="height: 120px; width:auto   ">
+					<img class="mb-2 mr-2" src="<?= base_url('assets/img/galeri/galeri(10).jpeg') ?>" alt=""
+						style="height: 120px; width:auto   ">
+				</div>
+			</div>
+		</div>
+
+	</div>
 </div>
